823f0a6b0a5e0f9eeed9efa5309a0261abedfc09
SergeyZakurakin <117861728+SergeyZakurakin@users.noreply.github.com>
2024-06-22 23:42:32 +0100

    setup project

---
+++ ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Sergey Zakurakin on 22/06/2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Sergey Zakurakin on 22/06/2024.
+//
+
+import Foundation
